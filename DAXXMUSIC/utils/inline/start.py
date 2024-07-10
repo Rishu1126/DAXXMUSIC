e2db914e2f7507b0b36d5adf78addf3d6c544841
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-07-10 12:06:29 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="𝗝𝗢𝗜𝗡 𝗢𝗨𝗥 𝗖𝗢𝗠𝗠𝗨𝗡𝗜𝗧𝗬", url=f"https://t.me/Stylish_Bio_Dp_0"),
         ],
     ]
     return buttons
@@ -20,13 +20,13 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text="𝗞𝗜𝗗𝗡𝗔𝗣 𝗠𝗘 🫣",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="👉🏻OWNER🥀", url=f"https://t.me/Niksonfire"),
+            InlineKeyboardButton(text="⚡𝗨𝗣𝗗𝗔𝗧𝗘𝗦⚡", url=f"https://t.me/Stylish_Bio_Dp_0"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
