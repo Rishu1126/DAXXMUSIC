d85d6bdae2b4b50b36e96435deecbd223e96b0ae
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-07-10 12:16:08 +0530

    Update buttons.py

--- DAXXMUSIC/utils/stuffs/buttons.py
+++ DAXXMUSIC/utils/stuffs/buttons.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters, enums 
 
 class BUTTONS(object):
-    MBUTTON = [[InlineKeyboardButton("CʜᴀᴛGPT", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("ɢʀᴏᴜᴘs", callback_data="mplus HELP_Group"),InlineKeyboardButton("sᴛɪᴄᴋᴇʀs", callback_data="mplus HELP_Sticker")],
+    MBUTTON = [[InlineKeyboardButton("⚜️𝗖𝗵𝗮𝘁𝗚𝗽𝘁⚜️", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("ɢʀᴏᴜᴘs", callback_data="mplus HELP_Group"),InlineKeyboardButton("sᴛɪᴄᴋᴇʀs", callback_data="mplus HELP_Sticker")],
     [InlineKeyboardButton("❣️𝗧𝗮𝗴-𝗔𝗹𝗹🥀", callback_data="mplus HELP_TagAll"),
     InlineKeyboardButton("❣️𝗶𝗻𝗳𝗼❣️", callback_data="mplus HELP_Info"),InlineKeyboardButton("Exᴛʀᴀ", callback_data="mplus HELP_Extra")],
     [InlineKeyboardButton("❣️𝗜𝗺𝗮𝗴𝗲🖼", callback_data="mplus HELP_Image"),
@@ -10,9 +10,9 @@ class BUTTONS(object):
     [InlineKeyboardButton("𝗙𝗼𝗻𝘁✨", callback_data="mplus HELP_Font"),
     InlineKeyboardButton("💟𝗚𝗮𝗺𝗲𝘀💟", callback_data="mplus HELP_Game"),InlineKeyboardButton("Ⓣ-ɢʀᴀᴘʜ", callback_data="mplus HELP_TG")],
     [InlineKeyboardButton("💘𝗶𝗺𝗽𝗼𝘀𝘁𝗲𝗿💘", callback_data="mplus HELP_Imposter"),
-    InlineKeyboardButton("Tʀᴜᴛʜ-ᗪᴀʀᴇ", callback_data="mplus HELP_TD"),InlineKeyboardButton("ʜᴀsᴛᴀɢ", callback_data="mplus HELP_HT")], 
-    [InlineKeyboardButton("ᴛᴛs", callback_data="mplus HELP_TTS"),
-    InlineKeyboardButton("ғᴜɴ", callback_data="mplus HELP_Fun"),InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q")],          
-    [InlineKeyboardButton("<", callback_data=f"settings_back_helper"), 
-    InlineKeyboardButton(">", callback_data=f"managebot123 settings_back_helper"),
+    InlineKeyboardButton("💛𝗧𝗿𝘂𝘁𝗵-Dare💛", callback_data="mplus HELP_TD"),InlineKeyboardButton("ʜᴀsᴛᴀɢ", callback_data="mplus HELP_HT")], 
+    [InlineKeyboardButton("💚𝗧𝘁𝘀💚", callback_data="mplus HELP_TTS"),
+    InlineKeyboardButton("💜𝗙𝘂𝗻💜", callback_data="mplus HELP_Fun"),InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q")],          
+    [InlineKeyboardButton("👈🏻", callback_data=f"settings_back_helper"), 
+    InlineKeyboardButton("👉🏻", callback_data=f"managebot123 settings_back_helper"),
     ]]
